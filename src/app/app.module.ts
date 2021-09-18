ba6b84fc69d8f0ce51529d66a0c5c4bfae58841c
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-09-18 17:26:35 +0530

    [SaiVijayaKumar]Adding:modules for note page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LoginComponent } from './Components/login/login.component';
 import {​​​​​​ FormsModule, ReactiveFormsModule }​​​​​​ from'@angular/forms';
-import { MatIconModule } from "@angular/material/icon";
 import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
-import { HttpClientModule } from '@angular/common/http';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
-import {​ FlexLayoutModule }​ from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
 
 
 @NgModule({
@@ -31,16 +24,9 @@ import {​ FlexLayoutModule }​ from '@angular/flex-layout';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
     FormsModule,
     ReactiveFormsModule,
-    MatIconModule,
-    HttpClientModule,
-    MatSnackBarModule,
-    FlexLayoutModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
