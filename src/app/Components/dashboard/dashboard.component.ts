89cafbc5ccefbd874863813736cb48409ea4989c
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-09-19 16:07:08 +0530

    [SaiVijayaKumar]Adding:user Profile

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   opened: boolean = true;
-  isGrid:boolean = false;
+  GridView:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
